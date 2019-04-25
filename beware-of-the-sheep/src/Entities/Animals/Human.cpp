81b92766e2e6f65423e57eec1c02c53a23fd92f7
Konstanty <kostu96@gmail.com>
2019-04-26 00:15:14 +0200

    WIP

--- beware-of-the-sheep/src/Entities/Animals/Human.cpp
+++ beware-of-the-sheep/src/Entities/Animals/Human.cpp
@@ -1,6 +1,6 @@
 #include "pch.h"
 #include "Human.h"
-
+#include "World.h"
 
 namespace bots {
 
@@ -10,7 +10,37 @@ namespace bots {
 
 	void Human::action()
 	{
+		std::string message;
+
 		int x = _getch();
+		if (x == 32 && m_specialSkillCooldownTimer == 0 && !m_isSpecialSkillOn) {
+			m_isSpecialSkillOn = true;
+			message = "Special skill was activated.";
+			m_world.addMessage(std::move(message));
+
+			x = _getch();
+		}
+
+		if (m_isSpecialSkillOn) {
+			message = "Special skill will be on for " + std::to_string(SPECIAL_SKILL_DURATION - m_specialSkillDurationTimer) + " turns.";
+			m_world.addMessage(std::move(message));
+			++m_specialSkillDurationTimer;
+		}
+
+		if (m_specialSkillDurationTimer >= SPECIAL_SKILL_DURATION) {
+			m_isSpecialSkillOn = false;
+			m_specialSkillCooldownTimer = SPECIAL_SKILL_COOLDOWN;
+			m_specialSkillDurationTimer = 0;
+			message = "Special skill was deactivated.";
+			m_world.addMessage(std::move(message));
+		}
+
+		if (m_specialSkillCooldownTimer > 0) {
+			message = "Special skill can't be used for " + std::to_string(m_specialSkillCooldownTimer) + " turns.";
+			m_world.addMessage(std::move(message));
+			--m_specialSkillCooldownTimer;
+		}
+
 		if (x == 224) {
 			int d = _getch();
 			if (d == 72)
--- beware-of-the-sheep/src/Entities/Animals/Human.h
+++ beware-of-the-sheep/src/Entities/Animals/Human.h
@@ -21,7 +21,11 @@ namespace bots {
 		inline Kind getKind() const override { return Kind::Human; }
 		inline std::string getClassName() const override { return "Human"; }
 	private:
-
+		static const unsigned int SPECIAL_SKILL_DURATION = 5;
+		static const unsigned int SPECIAL_SKILL_COOLDOWN = 5;
+		bool m_isSpecialSkillOn = false;
+		unsigned int m_specialSkillDurationTimer = 0;
+		unsigned int m_specialSkillCooldownTimer = 0;
 	};
 
 } // namespace bots
