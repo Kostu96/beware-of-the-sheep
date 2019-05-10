7d214c3ceaa8a30d9dfa4b5957302eb104efeabe
Konstanty <kostu96@gmail.com>
2019-05-10 13:53:45 +0200

    Add maven project

---
+++ src/main/java/com/kostu96/App.java
@@ -0,0 +1,14 @@
+package com.kostu96;
+
+import javax.swing.*;
+
+public final class App {
+    private App() {
+
+    }
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+    }
+}
