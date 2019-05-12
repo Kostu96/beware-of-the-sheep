7c43951753b85eebbcb70eb2a39f0839e3d48f4c
Konstanty <kostu96@gmail.com>
2019-05-12 23:32:54 +0200

    WIP on Plant and Animal class

--- src/com/kostu96/bots/entities/animals/Animal.java
+++ src/com/kostu96/bots/entities/animals/Animal.java
@@ -1,7 +1,25 @@
 package com.kostu96.bots.entities.animals;
 
+import java.util.Random;
+
+import com.kostu96.bots.World;
 import com.kostu96.bots.entities.Entity;
+import com.kostu96.bots.utils.Point;
 
 public abstract class Animal extends Entity {
 
+	public Animal(World world, Point position, int strength, int initiative) {
+		super(world, position, strength, initiative);
+	}
+
+	@Override
+	public void action() {
+		Random rand = new Random();
+		int vorh = rand.nextInt(1), norp = rand.nextInt(1);
+		
+		if (vorh == 1)
+			move(0, norp == 1 ? -1 : 1);
+		else
+			move(norp == 1 ? -1 : 1, 0);
+	}
 }
--- src/com/kostu96/bots/entities/plants/Plant.java
+++ src/com/kostu96/bots/entities/plants/Plant.java
@@ -1,7 +1,13 @@
 package com.kostu96.bots.entities.plants;
 
+import com.kostu96.bots.World;
 import com.kostu96.bots.entities.Entity;
+import com.kostu96.bots.utils.Point;
 
 public abstract class Plant extends Entity {
+
+	public Plant(World world, Point position, int strength) {
+		super(world, position, strength, 0);
+	}
 
 }
