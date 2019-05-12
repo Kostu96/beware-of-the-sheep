6937223c8b44ce9e44478859faec22ac2c010313
Konstanty <kostu96@gmail.com>
2019-05-12 23:36:57 +0200

    Add Fox class

---
+++ src/com/kostu96/bots/entities/animals/Fox.java
@@ -0,0 +1,18 @@
+package com.kostu96.bots.entities.animals;
+
+import com.kostu96.bots.World;
+import com.kostu96.bots.entities.Entity;
+import com.kostu96.bots.utils.Point;
+
+public class Fox extends Animal {
+
+	public Fox(World world, Point position, int strength, int initiative) {
+		super(world, position, strength, initiative);
+	}
+
+	@Override
+	public void collision(Entity other) {
+
+	}
+
+}
