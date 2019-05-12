3879c6d76f0acf9d7b96bfef75d975bd16f4b202
Konstanty <kostu96@gmail.com>
2019-05-12 23:39:14 +0200

    Add Belladonna class

---
+++ src/com/kostu96/bots/entities/plants/Belladonna.java
@@ -0,0 +1,20 @@
+package com.kostu96.bots.entities.plants;
+
+import com.kostu96.bots.World;
+import com.kostu96.bots.entities.Entity;
+import com.kostu96.bots.utils.Point;
+
+public class Belladonna extends Plant {
+
+	public Belladonna(World world, Point position, int strength) {
+		super(world, position, strength);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void collision(Entity other) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
