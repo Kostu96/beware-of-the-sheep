99f887c05408188b34a5dcc2f9b15434ba3b365b
Konstanty <kostu96@gmail.com>
2019-05-16 22:44:40 +0200

    WIP

--- src/com/kostu96/bots/World.java
+++ src/com/kostu96/bots/World.java
@@ -2,21 +2,28 @@ package com.kostu96.bots;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
+import com.kostu96.bots.entities.Entity;
+
 public class World extends JPanel {
 	private static final long serialVersionUID = 1L;
 	
 	private int columns, rows;
+	private List<Entity> entities;
 	
 	public World(int columns, int rows) throws IOException {
 		super();
 		
 		this.columns = columns;
 		this.rows = rows;
+		
+		entities = new ArrayList<>();
 		
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		for (int i = 0; i < rows; ++i) {
